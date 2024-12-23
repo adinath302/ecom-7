9ebe69825c927868e4c8dc4fe2bc438df625a28c
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-12-23 22:22:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TopProducts from "./components/TopProducts/TopProducts";
+import Banner from "./components/Banner/Banner";
+const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      // for animation
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Products />
+      <TopProducts />
+      <Banner />
+    </div>
+  );
+};
+
+export default App;
